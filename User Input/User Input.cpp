07bb553dbf4fb78b6e383cfddf597ea434a55cae
Blancdemon262 <noirangel626@gmail.com>
2025-01-07 08:10:55 +0000

    Add files via upload

---
+++ User Input/User Input.cpp
@@ -0,0 +1,31 @@
+// User Input.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int input1;
+	int input2;
+	char choice;
+
+	cout << "Please insert 1st value: " << endl;
+	cin >> input1;
+
+	cout << "Please insert 2nd value: " << endl;
+	cin >> input2;
+
+	do {
+		cout << "1 - Add" << endl;
+		cout << "2 - Multiply" << endl;
+		cout << "3 - Divide" << endl;
+		cout << "Q - Exit" << endl;
+		cout << " " << endl;
+		cout << "Please enter your choice:" << endl;
+		cin >> choice
+			switch (choice)
+			{
+			case "1":
+			}
+	}
+}
